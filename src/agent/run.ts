86a9fd5c86186f08a49eca5e3c39413090aedcb7
Yasemin Cüni <yasemin.cuni@flo.com.tr>
2026-04-09 21:47:41 +0300

    updates

--- src/agent/run.ts
+++ src/agent/run.ts
@@ -1,12 +1,16 @@
 import { generateText, type ModelMessage } from "ai";
 import { openai } from "@ai-sdk/openai";
-
+import { getTracer, Laminar } from "@lmnr-ai/lmnr";
 import { tools } from "./tools/index.ts";
 import { SYSTEM_PROMPT } from "./system/prompt.ts";
 
 import type { AgentCallbacks } from "../types.ts";
 
 const MODEL_NAME = "gpt-5-mini";
+
+Laminar.initialize({
+  projectApiKey: process.env.LMNR_PROJECT_API_KEY,
+});
 
 export async function runAgent(
   userMessage: string,
@@ -19,7 +23,11 @@ export async function runAgent(
     prompt: userMessage,
     system: SYSTEM_PROMPT,
     tools,
+    experimental_telemetry: {
+      isEnabled: true,
+      tracer: getTracer(),
+    }
   });
 
-  console.log(text);
+  console.log('done');
 }
